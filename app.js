5381ff6ca8009b941ddb60b8b4544b9efd144472
Rebeca Gomes <rebecagomesg12009@gmail.com>
2025-08-17 13:54:43 -0300

    Adicionado o app.js

--- app.js
+++ app.js
@@ -2,3 +2,25 @@
 
 import funcionarios from "./funcionario.json" with {type: "json"}
 
+function criarFuncionario(funcionario) {
+    const container = document.createElement('div')
+    container.className = 'container'
+
+    const foto = document.createElement('img')
+
+    const nome = document.createElement('span')
+    const cargo = document.createElement('p')
+
+    foto.src = `./img/${funcionario.imagem}`
+    nome.textContent = funcionario.nome
+    cargo.textContent = funcionario.cargo
+
+
+    body.appendChild(container)
+    container.appendChild(foto)
+    container.appendChild(nome)
+    container.appendChild(cargo)
+
+}
+
+funcionarios.forEach(criarFuncionario)
